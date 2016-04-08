dd6c47052969644ee7ee84a1423bf8e1441fbf7f
Pedro Carrillo <carrillochero6@gmail.com>
2016-04-08 16:06:26 -0500

    first design of account

---
+++ app/src/main/java/com/belatrixsf/allstars/ui/account/AccountPlaceholderActivity.java
@@ -0,0 +1,44 @@
+/* The MIT License (MIT)
+* Copyright (c) 2016 BELATRIX
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+package com.belatrixsf.allstars.ui.account;
+
+import android.os.Bundle;
+
+import com.belatrixsf.allstars.R;
+import com.belatrixsf.allstars.ui.common.BaseActivity;
+
+/**
+ * @author PedroCarrillo
+ * This activity must be removed or replaced using MVP. This is only for UI Mocking.
+ * TODO: delete this.
+ */
+public class AccountPlaceholderActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //TODO: activity_account should be move to fragment_account
+        setContentView(R.layout.activity_account);
+
+    }
+
+
+}
