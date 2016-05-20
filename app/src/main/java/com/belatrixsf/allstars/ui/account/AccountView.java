e6cbe7b827a005a8c895f6208f8f656a17e5959e
Pedro Carrillo <carrillochero6@gmail.com>
2016-05-20 13:39:46 -0500

    updating design

--- app/src/main/java/com/belatrixsf/allstars/ui/account/AccountView.java
+++ app/src/main/java/com/belatrixsf/allstars/ui/account/AccountView.java
@@ -34,7 +34,7 @@ public interface AccountView extends AllStarsView {
     void goSubCategoryDetail(Integer categoryId, Integer employeeId);
     void showCurrentMonthScore(String skypeId);
     void showScore(String score);
-    void showLocationFlag(String locationIcon);
+    void showLocation(String locationIcon);
     void showSubCategories(List<SubCategory> subCategories);
     void showLevel(String level);
     void showSkypeId(String skypeID);
