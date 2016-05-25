999ec909827e38708e224cad9774379665b841d4
Ivan Cerrate <ivan.ccerrate@gmail.com>
2016-05-25 15:32:10 -0500

    Keyword to Tag

--- app/src/main/java/com/belatrixsf/allstars/adapters/MainNavigationViewPagerAdapter.java
+++ app/src/main/java/com/belatrixsf/allstars/adapters/MainNavigationViewPagerAdapter.java
@@ -64,7 +64,7 @@ public class MainNavigationViewPagerAdapter extends FragmentPagerAdapter {
             case 2:
                 return "Contacts";
             default:
-                return "Keywords";
+                return "Tags";
         }
     }
 
