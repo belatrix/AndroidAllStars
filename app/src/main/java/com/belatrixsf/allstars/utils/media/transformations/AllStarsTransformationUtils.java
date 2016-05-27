97177414d2bb79cb9e2961504de5ed7a98856516
Ivan Cerrate <ivan.ccerrate@gmail.com>
2016-05-27 09:02:21 -0500

    Adding license

--- app/src/main/java/com/belatrixsf/allstars/utils/media/transformations/AllStarsTransformationUtils.java
+++ app/src/main/java/com/belatrixsf/allstars/utils/media/transformations/AllStarsTransformationUtils.java
@@ -18,7 +18,6 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
 */
-
 package com.belatrixsf.allstars.utils.media.transformations;
 
 import android.graphics.Bitmap;
