6b7dda5fe68b5506983705189589390b7c82cac5
Jo Yep <joyep@Jos-Mac-mini.local>
2016-05-30 13:12:45 -0500

    Fix automerge comments

--- app/src/androidTest/java/com/belatrixsf/allstars/TestFunction/TestFunction.java
+++ app/src/androidTest/java/com/belatrixsf/allstars/TestFunction/TestFunction.java
@@ -29,68 +29,76 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 
-public class TestFunction extends ViewActionUtils{
+public class TestFunction extends ViewActionUtils {
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    public void findIdAndWrite(int id, String keyName){
+    public void findIdAndWrite(int id, String keyName) {
         onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id))
-         .perform(typeText(keyName), closeSoftKeyboard());
+                .perform(typeText(keyName), closeSoftKeyboard());
     }
-    public void findIdAndWriteDoneEvent(int id, String keyName){
+
+    public void findIdAndWriteDoneEvent(int id, String keyName) {
         onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id))
                 .perform(typeText(keyName), pressImeActionButton());
     }
-    public void findIdAndTap(int id){
+
+    public void findIdAndTap(int id) {
         onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id)).perform(click());
     }
-    public void tapOnOverFlowMenu(){
+
+    public void tapOnOverFlowMenu() {
         openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
     }
-    public void findNameAndTap(int id){
+
+    public void findNameAndTap(int id) {
         onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withText(id)).perform(click());
     }
-    public void findTextAndTap(String keyName){
+
+    public void findTextAndTap(String keyName) {
         onView(isRoot()).perform(waitString(keyName, Constants.MILISECONDS));
         onView(ViewMatchers.withText(keyName)).perform(click());
     }
-    public void checktWithIdAndText(int id, String keyName){
+
+    public void checktWithIdAndText(int id, String keyName) {
         onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id)).check(matches(withText(keyName)));
     }
-    public void checkIfElementIsDisplayed(int id){
+
+    public void checkIfElementIsDisplayed(int id) {
         onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id)).check(matches(isDisplayed()));
     }
-    public void checkIfElementIsDisplayedWithText(String keyName){
+
+    public void checkIfElementIsDisplayedWithText(String keyName) {
         onView(ViewMatchers.withText(keyName)).check(matches(isDisplayed()));
     }
-    public void checkIfElementIsEnabled(int id){
+
+    public void checkIfElementIsEnabled(int id) {
         onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id)).check(matches(isEnabled()));
     }
-    public void checkIfElementIsChecked(int id){
+
+    public void checkIfElementIsChecked(int id) {
         onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id)).check(matches(isChecked()));
     }
+
     public void checkIfElementIsClickable(int id) {
         onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id)).check(matches(isClickable()));
     }
-    public void checkIfMatches(int idFirst, int idSecond){
+
+    public void checkIfMatches(int idFirst, int idSecond) {
         onView(isRoot()).perform(waitId(idSecond, Constants.MILISECONDS));
         onView(ViewMatchers.withId(idFirst)).check(matches(ViewMatchers.withId(idSecond)));
     }
-<<<<<<< HEAD
-=======
-    public void findItemOnRecyclerViewAndTap(int id, int position){
+    public void findItemOnRecyclerViewAndTap(int id, int position) {
         onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(withId(id)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
     }
-
-
->>>>>>> 191d125c60f82ddccd1187c5fa24d3d3da9e68d3
 }
+
