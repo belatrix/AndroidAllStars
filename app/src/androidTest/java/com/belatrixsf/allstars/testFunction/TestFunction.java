dc6c4f45e96875110baeae9612a964cb953d8501
Jo <jo.yep.arce@gmail.com>
2016-06-02 15:28:08 -0500

    "delete wait method"

--- app/src/androidTest/java/com/belatrixsf/allstars/testFunction/TestFunction.java
+++ app/src/androidTest/java/com/belatrixsf/allstars/testFunction/TestFunction.java
@@ -7,11 +7,6 @@ package com.belatrixsf.allstars.testFunction;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
-import android.widget.LinearLayout;
-
-import com.belatrixsf.allstars.R;
-import com.belatrixsf.allstars.util.Constants;
-import com.belatrixsf.allstars.util.ViewActionUtils;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
@@ -24,30 +19,25 @@ import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
-import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.IsNot.not;
 
 
-public class TestFunction extends ViewActionUtils {
+public class TestFunction {
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     public void findIdAndWrite(int id, String keyName) {
-        onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id))
                 .perform(typeText(keyName), closeSoftKeyboard());
     }
 
     public void findIdAndWriteDoneEvent(int id, String keyName) {
-        onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id))
                 .perform(typeText(keyName), pressImeActionButton());
     }
 
     public void findIdAndTap(int id) {
-        onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id)).perform(click());
     }
 
@@ -56,22 +46,18 @@ public class TestFunction extends ViewActionUtils {
     }
 
     public void findNameAndTap(int id) {
-        onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withText(id)).perform(click());
     }
 
     public void findTextAndTap(String keyName) {
-        onView(isRoot()).perform(waitString(keyName, Constants.MILISECONDS));
         onView(ViewMatchers.withText(keyName)).perform(click());
     }
 
     public void checktWithIdAndText(int id, String keyName) {
-        onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id)).check(matches(withText(keyName)));
     }
 
     public void checkIfElementIsDisplayed(int id) {
-        onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id)).check(matches(isDisplayed()));
     }
 
@@ -80,30 +66,23 @@ public class TestFunction extends ViewActionUtils {
     }
 
     public void checkIfElementIsEnabled(int id) {
-        onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id)).check(matches(isEnabled()));
     }
     public void checkIfElementIsNotEnabled(int id) {
-        onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id)).check(matches(not(isEnabled())));
     }
 
     public void checkIfElementIsChecked(int id) {
-        onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id)).check(matches(isChecked()));
     }
 
     public void checkIfElementIsClickable(int id) {
-        onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(ViewMatchers.withId(id)).check(matches(isClickable()));
     }
     public void checkIfMatches(int idFirst, int idSecond) {
-
-        onView(isRoot()).perform(waitId(idSecond, Constants.MILISECONDS));
         onView(ViewMatchers.withId(idFirst)).check(matches(ViewMatchers.withId(idSecond)));
     }
     public void findItemOnRecyclerViewAndTap(int id, int position) {
-        onView(isRoot()).perform(waitId(id, Constants.MILISECONDS));
         onView(withId(id)).perform(RecyclerViewActions.actionOnItemAtPosition(position, click()));
     }
 }
