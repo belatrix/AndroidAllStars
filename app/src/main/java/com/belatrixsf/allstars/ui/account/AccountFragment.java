7bc7e95090d12a839ef8c469524ec08050cb115c
Ivan Cerrate <ivan.ccerrate@gmail.com>
2016-06-10 11:47:05 -0500

    Expand profile picture implementation

--- app/src/main/java/com/belatrixsf/allstars/ui/account/AccountFragment.java
+++ app/src/main/java/com/belatrixsf/allstars/ui/account/AccountFragment.java
@@ -47,6 +47,7 @@ import com.belatrixsf.allstars.adapters.AccountSubCategoriesAdapter;
 import com.belatrixsf.allstars.entities.Employee;
 import com.belatrixsf.allstars.entities.SubCategory;
 import com.belatrixsf.allstars.ui.account.edit.EditAccountActivity;
+import com.belatrixsf.allstars.ui.account.expanded.ExpandPictureActivity;
 import com.belatrixsf.allstars.ui.common.AllStarsFragment;
 import com.belatrixsf.allstars.ui.common.RecyclerOnItemClickListener;
 import com.belatrixsf.allstars.ui.common.views.DividerItemDecoration;
@@ -62,11 +63,12 @@ import com.belatrixsf.allstars.utils.media.loaders.ImageLoader;
 import java.util.List;
 
 import butterknife.Bind;
+import butterknife.OnClick;
 
 import static com.belatrixsf.allstars.ui.account.AccountActivity.USER_ID_KEY;
 import static com.belatrixsf.allstars.ui.account.edit.EditAccountFragment.IS_NEW_USER;
-import static com.belatrixsf.allstars.ui.stars.GiveStarFragment.SELECTED_USER_KEY;
 import static com.belatrixsf.allstars.ui.account.edit.EditAccountFragment.RQ_EDIT_ACCOUNT;
+import static com.belatrixsf.allstars.ui.stars.GiveStarFragment.SELECTED_USER_KEY;
 
 /**
  * Created by pedrocarrillo on 4/9/16.
@@ -268,6 +270,16 @@ public class AccountFragment extends AllStarsFragment implements AccountView, Re
                 return false;
             }
         });
+    }
+
+    @OnClick(R.id.profile_picture)
+    public void profilePictureClicked() {
+        accountPresenter.profilePictureClicked();
+    }
+
+    @Override
+    public void goToExpandPhoto(String url) {
+        ExpandPictureActivity.startActivityAnimatingProfilePic(getActivity(), pictureImageView, url);
     }
 
     @Override
