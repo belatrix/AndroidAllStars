17dbec11415fb4f2bb7d49c2d46cd51bcdcc9d7f
Pedro Carrillo <carrillochero6@gmail.com>
2016-06-14 13:51:53 -0500

    Update MainActivity.java
    removing unused import

--- app/src/main/java/com/belatrixsf/allstars/ui/home/MainActivity.java
+++ app/src/main/java/com/belatrixsf/allstars/ui/home/MainActivity.java
@@ -33,7 +33,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.view.ActionMode;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
