d5f9a58d6d00cb04705bcd500d3acc00b50daff3
Ivan <ivan.ccerrate@gmail.com>
2016-07-18 17:07:45 -0500

    License added and logs removed

--- app/src/main/java/com/belatrixsf/connect/services/fcm/ConnectFirebaseMessagingService.java
+++ app/src/main/java/com/belatrixsf/connect/services/fcm/ConnectFirebaseMessagingService.java
@@ -1,3 +1,23 @@
+/* The MIT License (MIT)
+* Copyright (c) 2016 BELATRIX
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
 package com.belatrixsf.connect.services.fcm;
 
 import android.app.LauncherActivity;
@@ -8,7 +28,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import com.belatrixsf.connect.R;
 import com.belatrixsf.connect.managers.PreferencesManager;
@@ -23,21 +42,14 @@ import java.util.Map;
 
 public class ConnectFirebaseMessagingService extends FirebaseMessagingService {
 
-    private static final String TAG = "MyFirebaseMsgService";
-
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Map<String, String> notificationData = remoteMessage.getData();
         if (notificationData != null && PreferencesManager.get().isNotificationEnabled()) {
-            Log.d(TAG, "title: " + notificationData.get("title") + "");
-            Log.d(TAG, "detail " + remoteMessage.getData().get("detail"));
-
             sendNotification(notificationData.get("title"), remoteMessage.getData().get("detail"));
         }
     }
 
-    //This method is only generating push notification
-    //It is same as we did in earlier posts
     private void sendNotification(String messageTitle, String messageBody) {
         Intent intent = new Intent(this, LauncherActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
