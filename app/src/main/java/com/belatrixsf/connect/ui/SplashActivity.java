962a0c6e5b999f268e8dfde27f6f17c02059d969
Eduardo Chuquilin <educhuks@MacBook-Pro-de-Eduardo.local>
2017-09-11 02:48:40 -0500

    fixing status bar color

--- app/src/main/java/com/belatrixsf/connect/ui/SplashActivity.java
+++ app/src/main/java/com/belatrixsf/connect/ui/SplashActivity.java
@@ -20,11 +20,17 @@
 */
 package com.belatrixsf.connect.ui;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.TransitionDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
